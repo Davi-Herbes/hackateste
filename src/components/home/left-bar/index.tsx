62666be1164ff9c4b5bd1e44ad92ebae796e2295
Davi Herbes <hmatosdavi@gmail.com>
2025-03-31 08:36:31 -0300

    teste

---
+++ src/components/home/left-bar/index.tsx
@@ -0,0 +1,5 @@
+import { SLeftBar } from "./styles";
+
+export const LeftBar = () => {
+  return <SLeftBar className="left-bar"></SLeftBar>;
+};
---
+++ src/components/home/left-bar/styles.ts
@@ -0,0 +1,8 @@
+import styled, { css } from "styled-components";
+
+export const SLeftBar = styled.div`
+  ${({ theme }) => css`
+    .left-bar-element {
+    }
+  `}
+`;
